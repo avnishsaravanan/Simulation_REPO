6095759b29b21a841204d12f52ca1945aa77dc9e
AvnishSaravanan <avnish.saravanan@gmail.com>
2022-11-13 21:46:14 +0530

    added babylon and div list generator

--- basis.js
+++ basis.js
@@ -3,10 +3,9 @@
 //function beforeRender() to change position in simulation
 //function for converting between axes
 
-let BABYLON = require("https://cdn.babylonjs.com/babylon.js");
-
+let BABYLON = require("./node_modules/babylonjs");
 let inputParameters = [];
-let object;
+let object; 
 let objectCoordinate;
 let objects = [];
 let objectindex = 0;
@@ -14,11 +13,11 @@ let velocities = [];
 let masses = [];
 
 function axial_velocity(velocity, index) {
-    if (!!veloX) {veloX = velocity[0] * Math.cos(radians_degrees(velocity[1]))}
+    if (!!velocity[1]) {veloX = velocity[0] * Math.cos(radians_degrees(velocity[1]))}
     else {veloX = velocities[index].x};
-    if (!!veloY) {veloY = velocity[0] * Math.sin(radians_degrees(velocity[1]))}
+    if (!!velocity[1]) {veloY = velocity[0] * Math.sin(radians_degrees(velocity[1]))}
     else {veloY = velocities[index].y};
-    if (!!veloZ) {veloZ = velocity[0] * Math.sin(radians_degrees(velocity[2]))}
+    if (!!velocity[2]) {veloZ = velocity[0] * Math.sin(radians_degrees(velocity[2]))}
     else {veloZ = velocities[index].z}
     velocities[index] = {x : veloX, y : veloY, z : veloZ};
 }
@@ -31,9 +30,16 @@ function radians_degrees (input, path) {
         return pi * input/180 };
 }
 
-const engine = BABYLON.Engine
 const canvas = document.querySelector("#renderCanvas");
-const scene = BABYLON.Scene(canvas, true);
+const engine = BABYLON.Engine(canvas, true);
+
+create_obj = function (bodyName, parameters) {
+    const scene = new BABYLON.Scene(engine);
+    const camera = new BABYLON.ArcRotateCamera("camera1", 1, 1, 1, new BABYLON.Vector3.Zero(), scene);
+    const product = BABYLON.MeshBuilder.CreateSphere(bodyName, parameters, scene);
+    const light = new BABYLON.HemisphericLight ("light1", new BABYLON.Vector3(1, 1, 1), scene);
+    return scene;
+}
 
 function createObject (event) {
     event.preventDefault();
@@ -50,11 +56,12 @@ function createObject (event) {
     console.log(inputParameters[objectindex]);
 
     bodyName = parameters.elements["bodyname"].value;
-    object = BABYLON.MeshBuilder.CreateSphere(bodyName, {inputParameters}, scene);
-    //position
-    let objectCoordinate = new BABYLON.Vector3(parameters.elements["x"].value, parameters.elements["y"].value, parameters.elements["z"].value);
+    //object = BABYLON.MeshBuilder.CreateSphere(bodyName, {inputParameters}, scene);
+    objectCoordinate = new BABYLON.Vector3(parameters.elements["x"].value, parameters.elements["y"].value, parameters.elements["z"].value);
+        
+    object = create_obj(bodyName, inputParameters[objectindex]);
     object.position = objectCoordinate;
-    
+
     objects[objectindex] = object;
 
     velo = [parameters.elements["speed"].value, parameters.elements["angle_xy"].value, parameters.elements["angle_yz"].value];
@@ -66,20 +73,20 @@ function createObject (event) {
     console.log(velocities);
     masses[objectindex] = parameters.elements["mass"].value;
 
-    objects.push(object);
+    objectlist_gen((objectindex + 1));
     objectindex += 1;
-    return scene;
     }
 
-function objectlist () {
+function objectlist_gen (index) {
     let list = document.querySelector(".objects-list");
-    let classname = (`Object ${objectindex}`);
+    let classname = (`Object ${index}`);
     list.classList.add(classname);
-    let newclass = document.querySelector(`objects-list > ${newclass}`);
-    newclass.setAttribute("id", `${objectindex}`);
+    //declares class
+    let newclass = document.querySelector(`objects-list > ${classname}`);
+    newclass.setAttribute("id", objectindex);
     label = document.createElement("h3");
     newclass.appendChild(label);
-    label.innerHTML = (`Object ${objectindex}`);
+    label.innerHTML = (`Object ${index}`);
 
     /*alternate path:
     let list = document.querySelector("objects-list")
@@ -93,32 +100,42 @@ function editObject (event, editindex) {
     if (!!parameters.elements["size"].value) {
         inputParameters[editindex].diameter = parameters.elements["size"].value };
     if (!!parameters.elements["res"].value) {
-        inputParameters[editindex].segments = parameters["res"].value };
-    object = BABYLON.MeshBuilder.CreateSphere(bodyName, {inputParameters}, scene);
-    objects[editindex] = object;
-
+        inputParameters[editindex].segments = parameters.elements["res"].value };
+    
+    object = create_obj(bodyName, inputParameters[editindex]);
+    
     //test vector notation here:
     if (!!parameters.elements["x"].value) {
-        objects[editindex].position.x = parameters["x"].value };
+        object.position.x = parameters.elements["x"].value };
     if (!!parameters.elements["y"].value) {
-        objects[editindex].position.y = parameters.elements["y"].value };
+        object.position.y = parameters.elements["y"].value };
     if (!!parameters.elements["z"].value) {
-        objects[editindex].position.z = parameters["x"].value };
+        object.position.z = parameters.elements["z"].value };
     
-    velo = [parameters.element["speed"].value, parameters.element["angle_xy"].value, parameters.element["angle_zy"].value];
+    objects[editindex] = object;
+
+    velo = [parameters.elements["speed"].value, parameters.elements["angle_xy"].value, parameters.elements["angle_zy"].value];
     axial_velocity(velo, editindex);
     }
 
+    let divindex = 0;
+
 document.addEventListener("DOMContentLoaded", function() {
-    let detector1 = document.querySelector("#setParameters");
-    detector1.addEventListener("submit", function() {
+    let form1 = document.querySelector("#setParameters");
+    let form2 = document.querySelector("#editParameters");
+
+    form1.addEventListener("submit", function() {
         createObject(event);
     }) 
-    let detector2 = document.querySelector("#editParameters");
-    //create div list for all the objects
-    detector2.addEventListener("submit", function() {
-        editObject(event, selectedObj);
-
-
-    }) });
-
+        
+let objectslist = document.querySelector("objects-list");
+objectslist.forEach(function(object) {
+    //let divindex = objectslist.Array.from().indexOf(object);
+    object.addEventListener("click", function() {
+    form2.setAttribute("false", hidden);
+    form2.addEventListener("submit", function() {
+       editObject(event, divindex);
+       divindex += 1;
+    })
+    }) }) 
+});
