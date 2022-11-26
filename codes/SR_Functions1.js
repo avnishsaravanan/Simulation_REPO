29666a58a595817cc19e62ed08dace322fe1f43c
AvnishSaravanan <avnish.saravanan@gmail.com>
2022-11-26 23:14:56 +0530

    algorithm modifications, graphics untouched

--- codes/SR_Functions1.js
+++ codes/SR_Functions1.js
@@ -17,47 +17,49 @@ coaxial_velocity = velos.coaxial_velocity;
 displacement = velos.displacement;
 
 function equations (input) {
-    
+
+    this.content = {};    
     this.content = input; 
-    console.log(this.content);
+    console.log("from sr functions,", this.content);
 
     dt_P = this.content.dt_P;
     dt_Q = this.content.dt_Q;
+    console.log(this.content.dt_Q);
     dp_P = this.content.dp_P;
     dp_Q = this.content.dp_Q;
     mass1 = this.content.mass1;
     mass2 = this.content.mass2;
+    colinear_dis = this.content.colinear_dis;
     colinear_velo = this.content.colinear_velo;
     
-        
     velo_sum = ((colinear_velo.x**2) + (colinear_velo.y**2) + (colinear_velo.z**2));
     const LzF = (1/Math.sqrt(1 - (velo_sum/c**2))); //check whether Lorentz factor is directional
          
-    this.content.case1 = function case1() { //delta T in frame Q
+    this.case1 = function case1() { //delta T in frame Q
         //basic form of Lorentz transformation
         //account for asynchronised separation later
-        term1 = (colinear_velo.x * coaxial_displacement(dp_P.x));
-        term2 = (colinear_velo.y * coaxial_displacement(dp_P.y));
-        term3 = (colinear_velo.z * coaxial_displacement(dp_P.z));
+        term1 = (colinear_velo.x * colinear_dis.x);
+        term2 = (colinear_velo.y * colinear_dis.y);
+        term3 = (colinear_velo.z * colinear_dis.z);
         term4 = (dt_P + (-term1 - term2 - term3)/c**2);
         this.content.dt_Q = term4 * LzF; };
 
-    this.content.case2 = function case2() { //delta T in frame P
-        term1 = (colinear_velo.x * coaxial_displacement(dp_Q.x));
-        term2 = (colinear_veloY.y * coaxial_displacement(dp_Q.y));
-        term3 = ((colinear_velo.z * coaxial_displacement(dp_Q.z)));
+    this.case2 = function case2() { //delta T in frame P
+        term1 = (colinear_velo.x * colinear_dis.x);
+        term2 = (colinear_velo.y * colinear_dis.y);
+        term3 = (colinear_velo.z * colinear_dis.z);
         term4 = (dt_Q + (term1 + term2 + term3));
-        this.content.dt_Q = term4 * LzF; };
+        this.content.dt_P = term4 * LzF; };
     
-    this.content.case3 = function case3() {  //displacement in frame Q
+    this.case3 = function case3() {  //displacement in frame Q
         term1 = (dp_P.x - (colinear_velo.x * dt_P));
         term2 = (dp_P.y - (colinear_velo.y * dt_P));
         term3 = (dp_P.z - (colinear_velo.z * dt_P));
         term4 = [(term1 * LzF), (term2 * LzF), (term3 * LzF)];
-        this.content.dp_Q = displacement(term4, null).total;
+        this.content.dp_Q = displacement(term4).total;
         this.content.dp_P = dp_P.total; };
 
-    this.content.case4 = function case4() { //displacement in frame p
+    this.case4 = function case4() { //displacement in frame p
         term1 = (dp_Q.x - (colinear_velo.x * dt_Q));
         term2 = (dp_Q.y - (colinear_velo.y * dt_Q));
         term3 = (dp_Q.z - (colinear_velo.z * dt_Q));
@@ -65,23 +67,23 @@ function equations (input) {
         this.content.dp_P = displacement(term4).total;
         this.content.dp_Q = dp_Q.total; };
     
-    this.content.case6 = function case6() { //delta x' without delta x - assuming 100% coaxial velo
+    this.case6 = function case6() { //delta x' without delta x - assuming 100% coaxial velo
         term1 = ( LzF * - dt_P);
         term2 = (c**2/LzF * -(colinear_velo.total));
         this.content.dp_P = term2 * (dt_Q + term1); };
     
-    this.content.case5 = function case5() { //delta x without delta x' - assuming 100% coaxial velo
+    this.case5 = function case5() { //delta x without delta x' - assuming 100% coaxial velo
         term1 = ( LzF * - dt_Q);
         term2 = (c**2/LzF * (colinear_velo.total));
         this.content.dp_P = term2 * (dt_Q + term1); };
     
-    this.content.case7 = function case7() {
+    this.case7 = function case7() {
         this.content.energy1 = (mass1 * LzF * (c**2)); };
     
-    this.content.case8 = function case8() {
+    this.case8 = function case8() {
         this.content.energy2 = (mass2 * LzF * (c**2)); };
 
-    return this.content;
+    return this;
 }
 
 module.exports = equations;
--- codes/velocity_codes.js
+++ codes/velocity_codes.js
@@ -12,8 +12,8 @@ function radians_degrees (input, path) {
 
 function axial_velocity(velo) {
     let veloX = velo[0] * Math.cos(radians_degrees(velo[1]));
-    let veloY = velo[0] * Math.sin(radians_degrees(velo[1]));
-    let veloZ = velo[0] * Math.sin(radians_degrees(velo[2]));
+    let veloY = velo[0] * Math.sin(radians_degrees(velo[1])) * Math.sin(radians_degrees(velo[2]));
+    let veloZ = velo[0] * Math.sin(radians_degrees(velo[1])) * Math.cos(radians_degrees(velo[2]));
     console.log("done, from first call");
     return {x : veloX, y : veloY, z : veloZ};
 }
@@ -29,26 +29,72 @@ function displacement(term2, term1) {
 }
 
 function coaxial_displacement(relvelo, pos2, pos1) {
+    if (pos1[0] == null && pos2[0] == null) { return null }
+    else {
     coaxial_dis = {};
-    dis = displacement(pos2, pos1);
-    coaxial_dis.x = (relvelo.x/dis.y) / (relvelo.y/dis.x) * relvelo.x;
-    coaxial_dis.y = (relvelo.y/dis.x) / (relvelo.x/dis.y) * relvelo.y;
-    coaxial_dis.z = (relvelo.z/dis.y) / (relvelo.y/dis.z) * relvelo.z;
+    dis = displacement(pos2, pos1)
+    coefs = coaxial_velocity(relvelo, pos2, pos1);
+    coaxial_dis.x = coefs[0] * dis.x;
+    coaxial_dis.y = coefs[1] * dis.y;
+    coaxial_dis.z = coefs[2] * dis.z;
     coaxial_dis.total = Math.sqrt((coaxial_dis.x)**2 + (coaxial_dis.y)**2 + (coaxial_dis.z)**2);
+    return coaxial_dis }
 }
 
 function coaxial_velocity(relvelo, pos2, pos1) {
     coaxial_velo = {};
+    let coefx; let coefy; let coefz; let ratio1; let ratio2;
     dis = displacement(pos2, pos1);
-    if (!pos2[0] && !pos1[0]) { return null }
-    else {
-    coaxial_velo.x = (relvelo.x / dis.y) / (relvelo.y / dis.x) * relvelo.x;
-    coaxial_velo.y = (relvelo.y / dis.x) / (relvelo.x / dis.y) * relvelo.y;
-    coaxial_velo.z = (relvelo.z / dis.y) / (relvelo.y / dis.z) * relvelo.z;
+    if (pos2[0] == null && pos1[0] == null) { return null }
+
+    else { //conditional alg to prevent NaN
+
+           if (dis.x == 0) { coaxial_velo.x = 0; coefx = 0 } 
+           else { //block1
+                 if ((relvelo.x/relvelo.y) <= 1) { //block 1.1
+                    if ((dis.x/dis.y) <= (relvelo.x/relvelo.y)) { coefx = (dis.x/dis.y)/(relvelo.x/relvelo.y) }
+                    else {coefx = (dis.x/dis.y)/Math.tan(Math.atan(relvelo.x/relvelo.y) - Math.atan(dis.x/dis.y)) };
+                    }
+                 else { //block 1.2
+                    if ((dis.x/dis.y) <= (relvelo.x/relvelo.y)) { coefx = (dis.x/dis.y) / Math.tan(90 - Math.atan(relvelo.x/relvelo.y) - Math.atan(dis.x/dis.y))}
+                    else { coefx = (dis.x/dis.y)/(relvelo.x/relvelo.y) };
+                 }}
+    
+           if (dis.y == 0) { coaxial_velo.y = 0; coefy = 0 } 
+           else { //block 2
+                 ratio1 = (relvelo.y/relvelo.x) * (relvelo.y/relvelo.z);
+                 ratio2 = (dis.y/dis.x) * (dis.y/dis.z); 
+
+                 if (ratio1 >= 1) { //block 2.1
+                    if (ratio2 >= ratio1) { coefy = ratio2/ratio1 }
+                    else { coefy = ratio2 / Math.tan(Math.atan(ratio2) - Math.atan(ratio1)) }; 
+                   }
+                 else { //block 2.2
+                    if (ratio2 >= ratio1) { coefy = ratio2/Math.tan(90 - Math.atan(ratio2) - Math.atan(ratio1)) }
+                    else { coefy = ratio2/ratio1 };
+                 }}
+    
+            if (dis.z == 0) { coaxial_velo.z = 0; coefz = 0 }
+            else {//block 3
+                 ratio1 = (relvelo.z/relvelo.y) * (relvelo.z/relvelo.x);
+                 ratio2 = (dis.z/dis.y) * (dis.z/dis.x);
+                 if (ratio1 <= 1) { //block 3.1
+                    if (ratio2 <= ratio1) { coefz = ratio2/ratio1 }
+                    else { coefz = ratio1 / Math.tan(Math.atan(ratio2) - Math.atan(ratio1)) }; 
+                  }
+                 else { //block 3.2
+                   if (ratio2 <= ratio1) { coefz = ratio2 / Math.tan(90 - Math.atan(ratio2) - Math.atan(ratio1)) }
+                   else { coefz = ratio2 / ratio1 };
+              }}
+            }
+    coaxial_velo.x = relvelo.x * coefx;
+    coaxial_velo.y = relvelo.y * coefy;
+    coaxial_velo.z = relvelo.z * coefz;
     coaxial_velo.total = Math.sqrt((coaxial_velo.x)**2 + (coaxial_velo.y)**2 + (coaxial_velo.z)**2);
+    coaxial_velo.coefs = [coefx, coefy, coefz];
     console.log('from velocity codes', coaxial_velo);
     return coaxial_velo; }
-}
+
 module.exports = {coaxial_velocity: coaxial_velocity,
                   displacement: displacement,
                   coaxial_displacement: coaxial_displacement,
