bcbb3bbe834c1fd14ce421f21f848dd47a5148f5
Umaya15 <31180776+Umaya15@users.noreply.github.com>
2022-12-14 10:20:26 +0530

    UI Sync

--- result/customs.bundle.js
+++ result/customs.bundle.js
@@ -6,67 +6,131 @@
 
 
   //progress bar
-function progressbar(starttime) {
-    var i = 0;
-    function move() {
-        if (i == 0) {
-            i = 1;
-            var elem = document.getElementById("simBar");
-            var width = 10;
-            var id = setInterval(frame, 10);
-            function frame() {
-                if (width >= 100) {
-                    clearInterval(id);
-                    i = 0;
-                } else {
-                    width++;
-                    elem.style.width = width + "%";
-                    elem.innerHTML = width + "%";
-                }
-            }
-        }
+  function progressbar(factor,denom) {
+    var elem = document.getElementById("simBar");
+    var width = Math.round((factor/denom)*100,0);
+    if (width >= 100) {
+      elem.style.width = '100%';
+    } else {
+      elem.style.width = width + '%';
     }
-}
-
-
-// alert
-function CustomAlert(){
-    this.alert = function(message,title){
-      //document.body.innerHTML = document.body.innerHTML + '<div id="dialogoverlay"></div><div id="dialogbox" class="slit-in-vertical"><div><div id="dialogboxhead"></div><div id="dialogboxbody"></div><div id="dialogboxfoot"></div></div></div>';
-  
-      let dialogoverlay = document.getElementById('dialogoverlay');
-      let dialogbox = document.getElementById('dialogbox');
-      
-      let winH = window.innerHeight;
-      dialogoverlay.style.height = winH+"px";
-      
-      dialogbox.style.top = "100px";
-  
-      dialogoverlay.style.display = "block";
-      dialogbox.style.display = "block";
-      
-      document.getElementById('dialogboxhead').style.display = 'block';
-  
-      if(typeof title === 'undefined') {
-        document.getElementById('dialogboxhead').style.display = 'none';
-      } else {
-        document.getElementById('dialogboxhead').innerHTML = '<i class="fa fa-exclamation-circle" aria-hidden="true"></i> '+ title;
-      }
-      document.getElementById('dialogboxbody').innerHTML = message;
-      document.getElementById('dialogboxfoot').innerHTML = '<button class="pure-material-button-contained active" id="okbtn">OK</button>';
-    };
-    
-    this.ok = function(){
-      document.getElementById('dialogbox').style.display = "none";
-      document.getElementById('dialogoverlay').style.display = "none";
-    };
   }
-
   
+  //timer
+  class simTimer {
+    constructor () {
+      this.isRunning = false;
+      this.startTime = 0;
+      this.overallTime = 0;
+    }
   
-  //let customAlert = new CustomAlert();
-
-module.exports = {progressbar: progressbar, CustomAlert: CustomAlert};
+    _getTimeElapsedSinceLastStart () {
+      if (!this.startTime) {
+        return 0;
+      }
+    
+      return Date.now() - this.startTime;
+    }
+  
+    simtimestart () {
+      if (this.isRunning) {
+        return console.error('Timer is already running');
+      }
+  
+      this.isRunning = true;
+  
+      this.startTime = Date.now();
+    }
+  
+    simtimestop () {
+      if (!this.isRunning) {
+        return console.error('Timer is already stopped');
+      }
+  
+      this.isRunning = false;
+  
+      this.overallTime = this.overallTime + this._getTimeElapsedSinceLastStart();
+    }
+  
+    simtimereset () {
+      this.overallTime = 0;
+  
+      if (this.isRunning) {
+        this.startTime = Date.now();
+        return;
+      }
+  
+      this.startTime = 0;
+    }
+  
+    getsimtime () {
+      if (!this.startTime) {
+        return 0;
+      }
+  
+      if (this.isRunning) {
+        return this.overallTime + this._getTimeElapsedSinceLastStart();
+      }
+  
+      return this.overallTime;
+    }
+  
+    setsimtime (timelim) {
+      let tl = ""; let ts ="";
+      let timeInSeconds = Math.round(this.getsimtime() / 1000);
+      if (timelim>9) { tl = "0:"+timelim; } else { tl = "0:0"+timelim; }
+      if (timeInSeconds>timelim) { timeInSeconds=timelim; }
+      if (timeInSeconds>9) { ts = "0:"+timeInSeconds; } else { ts = "0:0"+timeInSeconds; }
+      document.getElementById('simtimeinfo').innerText = ts+"/"+tl;
+      progressbar(timeInSeconds,timelim);
+    }
+  
+    /*setInterval(() => {
+      const timeInSeconds = Math.round(timer.getTime() / 1000);
+      document.getElementById('time').innerText = timeInSeconds;
+    }, 100)*/
+  }
+  const simtimer = new simTimer();
+  
+  // alert
+  function CustomAlert(){
+      this.alert = function(message,title){
+        //document.body.innerHTML = document.body.innerHTML + '<div id="dialogoverlay"></div><div id="dialogbox" class="slit-in-vertical"><div><div id="dialogboxhead"></div><div id="dialogboxbody"></div><div id="dialogboxfoot"></div></div></div>';
+    
+        let dialogoverlay = document.getElementById('dialogoverlay');
+        let dialogbox = document.getElementById('dialogbox');
+        
+        let winH = window.innerHeight;
+        dialogoverlay.style.height = winH+"px";
+        
+        dialogbox.style.top = "100px";
+    
+        dialogoverlay.style.display = "block";
+        dialogbox.style.display = "block";
+        
+        document.getElementById('dialogboxhead').style.display = 'block';
+    
+        if(typeof title === 'undefined') {
+          document.getElementById('dialogboxhead').style.display = 'none';
+        } else {
+          document.getElementById('dialogboxhead').innerHTML = '<i class="fa fa-exclamation-circle" aria-hidden="true"></i> '+ title;
+        }
+        document.getElementById('dialogboxbody').innerHTML = message;
+        document.getElementById('dialogboxfoot').innerHTML = '<button class="pure-material-button-contained active" id="okbtn">OK</button>';
+      };
+      
+      this.ok = function(){
+        document.getElementById('dialogbox').style.display = "none";
+        document.getElementById('dialogoverlay').style.display = "none";
+      };
+    }
+    const newalert = new CustomAlert();
+  
+    
+    
+    //let customAlert = new CustomAlert();
+  
+  module.exports = {progressbar: progressbar, CustomAlert: CustomAlert, newalert: newalert, simtimer: simtimer, simTimer: simTimer};
 
 /***/ })
 
--- result/render1.bundle.js
+++ result/render1.bundle.js
@@ -127,7 +127,6 @@ let velos = __webpack_require__(5);
 let raddeg = velos.radians_degrees;
 let axial_velocity = velos.axial_velocity;
 let displacement = velos.displacement;
-
 let checked = [0, 1];
 
 function synthObject (scene, objspecs, synthindex) {
@@ -306,8 +305,15 @@ function render (masses, velo, positions, array, timelim2) {
 
     let toRender = createScene();
     engine.runRenderLoop(function () {
-        if ((timetrack >= timelim2) || off == true) { engine.stopRenderLoop(); }
-        else { toRender.render(); }} );
+        timetrack += 1/60;
+        if (timetrack >= timelim) { 
+            custom.simtimer.simtimestop();
+            engine.stopRenderLoop(); 
+        } else { 
+            custom.simtimer.setsimtime(timelim);
+            toRender.render();
+        }
+    })
     
     window.addEventListener("resize", function () {
         engine.resize();
@@ -323,67 +329,131 @@ function render (masses, velo, positions, array, timelim2) {
 
 
   //progress bar
-function progressbar(starttime) {
-    var i = 0;
-    function move() {
-        if (i == 0) {
-            i = 1;
-            var elem = document.getElementById("simBar");
-            var width = 10;
-            var id = setInterval(frame, 10);
-            function frame() {
-                if (width >= 100) {
-                    clearInterval(id);
-                    i = 0;
-                } else {
-                    width++;
-                    elem.style.width = width + "%";
-                    elem.innerHTML = width + "%";
-                }
-            }
-        }
+  function progressbar(factor,denom) {
+    var elem = document.getElementById("simBar");
+    var width = Math.round((factor/denom)*100,0);
+    if (width >= 100) {
+      elem.style.width = '100%';
+    } else {
+      elem.style.width = width + '%';
     }
-}
-
-
-// alert
-function CustomAlert(){
-    this.alert = function(message,title){
-      //document.body.innerHTML = document.body.innerHTML + '<div id="dialogoverlay"></div><div id="dialogbox" class="slit-in-vertical"><div><div id="dialogboxhead"></div><div id="dialogboxbody"></div><div id="dialogboxfoot"></div></div></div>';
-  
-      let dialogoverlay = document.getElementById('dialogoverlay');
-      let dialogbox = document.getElementById('dialogbox');
-      
-      let winH = window.innerHeight;
-      dialogoverlay.style.height = winH+"px";
-      
-      dialogbox.style.top = "100px";
-  
-      dialogoverlay.style.display = "block";
-      dialogbox.style.display = "block";
-      
-      document.getElementById('dialogboxhead').style.display = 'block';
-  
-      if(typeof title === 'undefined') {
-        document.getElementById('dialogboxhead').style.display = 'none';
-      } else {
-        document.getElementById('dialogboxhead').innerHTML = '<i class="fa fa-exclamation-circle" aria-hidden="true"></i> '+ title;
-      }
-      document.getElementById('dialogboxbody').innerHTML = message;
-      document.getElementById('dialogboxfoot').innerHTML = '<button class="pure-material-button-contained active" id="okbtn">OK</button>';
-    };
-    
-    this.ok = function(){
-      document.getElementById('dialogbox').style.display = "none";
-      document.getElementById('dialogoverlay').style.display = "none";
-    };
   }
-
   
+  //timer
+  class simTimer {
+    constructor () {
+      this.isRunning = false;
+      this.startTime = 0;
+      this.overallTime = 0;
+    }
   
-  //let customAlert = new CustomAlert();
-
-module.exports = {progressbar: progressbar, CustomAlert: CustomAlert};
+    _getTimeElapsedSinceLastStart () {
+      if (!this.startTime) {
+        return 0;
+      }
+    
+      return Date.now() - this.startTime;
+    }
+  
+    simtimestart () {
+      if (this.isRunning) {
+        return console.error('Timer is already running');
+      }
+  
+      this.isRunning = true;
+  
+      this.startTime = Date.now();
+    }
+  
+    simtimestop () {
+      if (!this.isRunning) {
+        return console.error('Timer is already stopped');
+      }
+  
+      this.isRunning = false;
+  
+      this.overallTime = this.overallTime + this._getTimeElapsedSinceLastStart();
+    }
+  
+    simtimereset () {
+      this.overallTime = 0;
+  
+      if (this.isRunning) {
+        this.startTime = Date.now();
+        return;
+      }
+  
+      this.startTime = 0;
+    }
+  
+    getsimtime () {
+      if (!this.startTime) {
+        return 0;
+      }
+  
+      if (this.isRunning) {
+        return this.overallTime + this._getTimeElapsedSinceLastStart();
+      }
+  
+      return this.overallTime;
+    }
+  
+    setsimtime (timelim) {
+      let tl = ""; let ts ="";
+      let timeInSeconds = Math.round(this.getsimtime() / 1000);
+      if (timelim>9) { tl = "0:"+timelim; } else { tl = "0:0"+timelim; }
+      if (timeInSeconds>timelim) { timeInSeconds=timelim; }
+      if (timeInSeconds>9) { ts = "0:"+timeInSeconds; } else { ts = "0:0"+timeInSeconds; }
+      document.getElementById('simtimeinfo').innerText = ts+"/"+tl;
+      progressbar(timeInSeconds,timelim);
+    }
+  
+    /*setInterval(() => {
+      const timeInSeconds = Math.round(timer.getTime() / 1000);
+      document.getElementById('time').innerText = timeInSeconds;
+    }, 100)*/
+  }
+  const simtimer = new simTimer();
+  
+  // alert
+  function CustomAlert(){
+      this.alert = function(message,title){
+        //document.body.innerHTML = document.body.innerHTML + '<div id="dialogoverlay"></div><div id="dialogbox" class="slit-in-vertical"><div><div id="dialogboxhead"></div><div id="dialogboxbody"></div><div id="dialogboxfoot"></div></div></div>';
+    
+        let dialogoverlay = document.getElementById('dialogoverlay');
+        let dialogbox = document.getElementById('dialogbox');
+        
+        let winH = window.innerHeight;
+        dialogoverlay.style.height = winH+"px";
+        
+        dialogbox.style.top = "100px";
+    
+        dialogoverlay.style.display = "block";
+        dialogbox.style.display = "block";
+        
+        document.getElementById('dialogboxhead').style.display = 'block';
+    
+        if(typeof title === 'undefined') {
+          document.getElementById('dialogboxhead').style.display = 'none';
+        } else {
+          document.getElementById('dialogboxhead').innerHTML = '<i class="fa fa-exclamation-circle" aria-hidden="true"></i> '+ title;
+        }
+        document.getElementById('dialogboxbody').innerHTML = message;
+        document.getElementById('dialogboxfoot').innerHTML = '<button class="pure-material-button-contained active" id="okbtn">OK</button>';
+      };
+      
+      this.ok = function(){
+        document.getElementById('dialogbox').style.display = "none";
+        document.getElementById('dialogoverlay').style.display = "none";
+      };
+    }
+    const newalert = new CustomAlert();
+  
+    
+    
+    //let customAlert = new CustomAlert();
+  
+  module.exports = {progressbar: progressbar, CustomAlert: CustomAlert, newalert: newalert, simtimer: simtimer, simTimer: simTimer};
 
 /***/ })
 /******/ 	]);
